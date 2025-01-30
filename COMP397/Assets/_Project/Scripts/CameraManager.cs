6e265b0f104a172543aca70f7739bd290b78eec0
Ido Isaac <Idoisaac93@gmail.com>
2025-01-29 20:08:15 -0500

    Lab4

---
+++ COMP397/Assets/_Project/Scripts/CameraManager.cs
@@ -0,0 +1,24 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace COMP397
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [SerializeField] private CinemachineCamera freeLookCam;
+        [SerializeField] private Transform playerTransform;
+
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if (playerTransform != null) { return; }
+            playerTransform = GameObject.FindWithTag("Player").transform;
+        }
+
+            private void OnEnable()
+        {
+            freeLookCam.Target.TrackingTarget = playerTransform;
+        }
+    }
+}
---
+++ COMP397/Assets/_Project/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace COMP397
+{
+    public class EnemyController : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+
+        private void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+        private void Update()
+        {
+            var destination = GameObject.FindWithTag("Player").transform.position;
+            agent.destination = destination;
+        }
+    }
+}
