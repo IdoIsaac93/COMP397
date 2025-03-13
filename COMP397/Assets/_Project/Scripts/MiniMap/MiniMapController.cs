cd73dc5399f8ab973c695a15c6f4e5822022dfc7
Ido Isaac <Idoisaac93@gmail.com>
2025-03-12 20:03:06 -0400

    Lab 7

---
+++ COMP397/Assets/_Project/Scripts/MiniMap/MiniMapController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace COMP397
+{
+    public class MiniMapController : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+
+        private void Start()
+        {
+            if (player != null) { return; }
+            player = GameObject.FindWithTag("Player").transform;
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+        }
+    }
+}
